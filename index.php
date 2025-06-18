09c266eaea3f1ec8dc2a7edf5e0f56f07b34e8c6
Highfren <lordhigfren@gmail.com>
2025-06-18 11:51:48 +0100

    Primer_Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Este código es HTML...</h1>
+    <?php
+    echo "<h2>...y este código es PHP </h2>";
+    echo "<p style='color: red;'>esto es una prueba</p>";
+
+    ?>
+</body>
+
+</html>
